b30b558cce9b156ec14485596189274fcb2dede0
Valeria D <vdaffarra@gmail.com>
2023-05-31 10:45:24 -0400

    single quote

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Link from "next/link";
+import Link from 'next/link';
 import { motion, AnimatePresence } from 'framer-motion'
 const navigationVariants = {
   initial: {
